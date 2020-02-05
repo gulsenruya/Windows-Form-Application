61f9a52e1c8b305e822ef6ea6a3a4dcfba2ff0a2
gulsenruya <60214353+gulsenruya@users.noreply.github.com>
2020-02-05 10:53:40 +0300

    Sayisal Loto

---
+++ WFA_SayisalLoto/WFA_SayisalLoto/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace WFA_SayisalLoto
+{
+    partial class btnkontrolEt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.listBox6 = new System.Windows.Forms.ListBox();
+            this.listBox7 = new System.Windows.Forms.ListBox();
+            this.listBox8 = new System.Windows.Forms.ListBox();
+            this.btnSayiUret = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnKontrlEt = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(48, 76);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(190, 238);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(244, 75);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(190, 238);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(440, 76);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(190, 238);
+            this.listBox3.TabIndex = 2;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(636, 75);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(190, 238);
+            this.listBox4.TabIndex = 3;
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.Location = new System.Drawing.Point(48, 320);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(190, 238);
+            this.listBox5.TabIndex = 4;
+            // 
+            // listBox6
+            // 
+            this.listBox6.FormattingEnabled = true;
+            this.listBox6.Location = new System.Drawing.Point(244, 320);
+            this.listBox6.Name = "listBox6";
+            this.listBox6.Size = new System.Drawing.Size(190, 238);
+            this.listBox6.TabIndex = 5;
+            // 
+            // listBox7
+            // 
+            this.listBox7.FormattingEnabled = true;
+            this.listBox7.Location = new System.Drawing.Point(440, 320);
+            this.listBox7.Name = "listBox7";
+            this.listBox7.Size = new System.Drawing.Size(190, 238);
+            this.listBox7.TabIndex = 6;
+            // 
+            // listBox8
+            // 
+            this.listBox8.FormattingEnabled = true;
+            this.listBox8.Location = new System.Drawing.Point(636, 320);
+            this.listBox8.Name = "listBox8";
+            this.listBox8.Size = new System.Drawing.Size(190, 238);
+            this.listBox8.TabIndex = 7;
+            // 
+            // btnSayiUret
+            // 
+            this.btnSayiUret.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSayiUret.Location = new System.Drawing.Point(269, 565);
+            this.btnSayiUret.Name = "btnSayiUret";
+            this.btnSayiUret.Size = new System.Drawing.Size(361, 44);
+            this.btnSayiUret.TabIndex = 8;
+            this.btnSayiUret.Text = "SAYI ÜRET";
+            this.btnSayiUret.UseVisualStyleBackColor = true;
+            this.btnSayiUret.Click += new System.EventHandler(this.BtnSayiUret_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(135, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 64);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "10";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(239, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 64);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "10";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(337, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 64);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "10";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(438, 3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 64);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "10";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(533, 3);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 64);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "10";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnKontrlEt
+            // 
+            this.btnKontrlEt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKontrlEt.Location = new System.Drawing.Point(745, 12);
+            this.btnKontrlEt.Name = "btnKontrlEt";
+            this.btnKontrlEt.Size = new System.Drawing.Size(140, 48);
+            this.btnKontrlEt.TabIndex = 14;
+            this.btnKontrlEt.Text = "KONTROL ET";
+            this.btnKontrlEt.UseVisualStyleBackColor = true;
+            this.btnKontrlEt.Click += new System.EventHandler(this.BtnKontrlEt_Click);
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(631, 3);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 64);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "10";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(12, 12);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 18);
+            this.label7.TabIndex = 16;
+            // 
+            // btnkontrolEt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Purple;
+            this.ClientSize = new System.Drawing.Size(922, 614);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnKontrlEt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSayiUret);
+            this.Controls.Add(this.listBox8);
+            this.Controls.Add(this.listBox7);
+            this.Controls.Add(this.listBox6);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "btnkontrolEt";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.ListBox listBox6;
+        private System.Windows.Forms.ListBox listBox7;
+        private System.Windows.Forms.ListBox listBox8;
+        private System.Windows.Forms.Button btnSayiUret;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnKontrlEt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ WFA_SayisalLoto/WFA_SayisalLoto/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA_SayisalLoto
+{
+    public partial class btnkontrolEt : Form
+    {
+        public btnkontrolEt()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+        int[] sayilar = new int[6];       
+        private void BtnSayiUret_Click(object sender, EventArgs e)
+        {
+            foreach (Control ctrl in this.Controls)
+            {
+
+                if (ctrl is ListBox)
+                {
+
+                    int sayac = 0;
+                    ListBox listBox = (ListBox)ctrl;
+                    listBox.Items.Clear();
+                    while (sayac < 6)
+                    {
+                        int sayi = rnd.Next(1, 49);
+                        if (!listBox.Items.Contains(sayi))
+                        {
+                            listBox.Items.Add(sayi);
+
+                            sayac++;
+
+
+                        }
+                    }
+
+
+                    for (int i = 0; i < 6; i++)
+                    {
+                        int sayi1 = rnd.Next(1, 49);
+                        sayilar[i] = sayi1;
+                    }
+                    label1.Text = sayilar[0].ToString();
+                    label2.Text = sayilar[1].ToString();
+                    label3.Text = sayilar[2].ToString();
+                    label4.Text = sayilar[3].ToString();
+                    label5.Text = sayilar[4].ToString();
+                    label6.Text = sayilar[5].ToString();
+                    
+
+
+                }
+
+            }
+        }
+
+        private void BtnKontrlEt_Click(object sender, EventArgs e)
+        {
+            int sayac = 0;
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+
+                if (listBox1.Items.Contains(sayilar[i]) || listBox2.Items.Contains(sayilar[i]) || listBox3.Items.Contains(sayilar[i]) || listBox4.Items.Contains(sayilar[i]) || listBox5.Items.Contains(sayilar[i]) || listBox6.Items.Contains(sayilar[i]) || listBox7.Items.Contains(sayilar[i]) || listBox8.Items.Contains(sayilar[i]))
+                {
+                    sayac++;
+
+
+                }
+            }
+            label7.Text = sayac.ToString();
+            if (sayac == 6)
+            {
+                MessageBox.Show("TEBRIKLER KAZANDINIZ :)) ");
+            }
+            else
+            {
+                MessageBox.Show("Şans sizden yana değill :( ");
+            }
+        }
+    }
+}
